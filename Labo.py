3abef3f576ee961f8f8a33d3282f2b6fefa323e6
Jehod <nicolas.rochas.alpes@gmail.com>
2020-07-02 12:38:45 +0200

    structure 1

---
+++ Labo.py
@@ -0,0 +1,17 @@
+import os
+
+
+def init():
+    pass
+
+
+def stop_process():
+    """
+    action to end
+    """
+    os.system(" pause ")
+
+
+if __name__ == '__main__':
+    init()
+    stop_process()
